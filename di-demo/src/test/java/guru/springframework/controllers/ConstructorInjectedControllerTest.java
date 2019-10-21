48790c1a80597ac45d17ace7baa385c30dd5d84f
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2019-10-20 21:37:11 -0300

    Adding controllers and tests

---
+++ di-demo/src/test/java/guru/springframework/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+public class ConstructorInjectedControllerTest {
+    private ConstructorInjectedController constructorInjectedController;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, constructorInjectedController.sayHello());
+    }
+}
---
+++ di-demo/src/test/java/guru/springframework/controllers/GetterInjectedControllerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+public class GetterInjectedControllerTest {
+
+    private GetterInjectedController getterInjectedController;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.getterInjectedController = new GetterInjectedController();
+        this.getterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, getterInjectedController.sayHello());
+    }
+}
