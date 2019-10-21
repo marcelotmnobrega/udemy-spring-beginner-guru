c6909b775064bef5893b9f0dd4ce9d8a0a5cd8e2
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2019-10-20 22:47:54 -0300

    Refactoring controllers and tests

--- di-demo/src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ di-demo/src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -15,6 +15,7 @@ public class PropertyInjectedController {
     @Autowired
     //This time we're using the field as the implementation name (bean name)
     //But after we created the @Primary, the Primary is being used.
+    //If you want to use the GreetingServiceImpl YOU NEED TO USE the @Qualifier("greetingServiceImpl")
     public GreetingService greetingServiceImpl;
 
     public String sayHello(){
